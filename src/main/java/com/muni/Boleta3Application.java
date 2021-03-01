46093274480d37d546af4e89e0edc6f8ca5d5919
Gustavo <gustavo.iglesias40@gmai.com>
2021-03-01 14:56:23 -0300

    botonpago

--- src/main/java/com/muni/Boleta3Application.java
+++ src/main/java/com/muni/Boleta3Application.java
@@ -2,6 +2,7 @@ package com.muni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Boleta3Application {
@@ -9,5 +10,5 @@ public class Boleta3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Boleta3Application.class, args);
 	}
-
+	
 }
