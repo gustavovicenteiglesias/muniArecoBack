f781b71f33441b70def433fec4fba7c262c9711f
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2021-03-15 15:04:04 -0300

    autoadministrable

--- src/main/java/com/muni/Boleta3Application.java
+++ src/main/java/com/muni/Boleta3Application.java
@@ -1,12 +1,21 @@
 package com.muni;
 
+
+
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.muni.service.FilesStorageService;
+
+
 
 
 @SpringBootApplication
 public class Boleta3Application {
-
+	@Resource
+	 FilesStorageService storageService;
 	public static void main(String[] args) {
 		SpringApplication.run(Boleta3Application.class, args);
 	}
