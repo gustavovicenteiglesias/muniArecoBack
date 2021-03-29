5076784f4145db5d388419719f6cbffe03bbb08d
Gustavo <gviglesias@alumnos.unsada.edu.ar>
2021-03-29 12:35:00 -0300

    # WARNING: head commit changed in the meantime
    autoadministrable

--- src/main/java/com/muni/security/WebSecurityConfig.java
+++ src/main/java/com/muni/security/WebSecurityConfig.java
@@ -61,6 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/bunica/**").permitAll()
+			.antMatchers("/api/boton/**").permitAll()
 			.antMatchers("/api/comercio/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.antMatchers("/api/home/imagen/**").permitAll()
